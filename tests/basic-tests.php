680e4b156f865f3a0edd409b46aaf82a6b1d5891
tellyworth <tellyworth@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-30 07:29:24 +0000

    break up the tests a little
    git-svn-id: https://plugins.svn.wordpress.org/akismet@295113 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tests/basic-tests.php
+++ tests/basic-tests.php
@@ -21,7 +21,6 @@ class TestAkismetRetry extends UnitTestCase {
 		// and update commentmeta accordingly
 		akismet_auto_check_update_meta( $this->comment_id, $comment );
 		
-		
 
 		$akismet_last_comment = null;
 	}
@@ -30,21 +29,29 @@ class TestAkismetRetry extends UnitTestCase {
 		wp_delete_comment( $this->comment_id );
 	}
 	
-	function test_retry() {
-		
-		// first make sure the comment is in the correct state
+	function test_state_before_retry() {
+		// make sure the comment is in the correct state
 		$this->assertTrue( 0 < get_comment_meta( $this->comment_id, 'akismet_error', true ) );
 		$this->assertFalse( get_comment_meta( $this->comment_id, 'akismet_result', true ) );
-		
+	}
+	
+	function test_history_before_retry() {
+
 		// history should record a check-error
 		$history = akismet_get_comment_history( $this->comment_id );
 		$this->assertEqual( 'check-error', $history[0]['event'] );
-		
+	}
+	
+	function test_state_after_retry() {
 		// trigger a cron event and make sure the error status is replaced with 'false' (not spam)
 		akismet_cron_recheck( 0 );
 		
 		$this->assertFalse( get_comment_meta( $this->comment_id, 'akismet_error', true ) );
 		$this->assertEqual( 'false', get_comment_meta( $this->comment_id, 'akismet_result', true ) );
+	}
+	
+	function test_history_after_retry() {
+		akismet_cron_recheck( 0 );
 
 		// history should record a retry
 		$history = akismet_get_comment_history( $this->comment_id );
