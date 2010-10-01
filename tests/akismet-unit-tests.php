f977205b860b00686e58f6786d2409235c352057
tellyworth <tellyworth@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-01 00:41:17 +0000

    require WP_DEBUG, warn against running this on a production site
    git-svn-id: https://plugins.svn.wordpress.org/akismet@295443 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tests/akismet-unit-tests.php
+++ tests/akismet-unit-tests.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Akismet Unit Tests
 Plugin URI: http://akismet.com/
-Description: Additional code to help while testing the Akismet plugin
+Description: Additional code to help while testing the Akismet plugin.  DO NOT RUN THIS ON A PRODUCTION SITE!
 Version: 2.4.0
 Author: Automattic
 Author URI: http://automattic.com/wordpress-plugins/
@@ -17,6 +17,22 @@ function akismet_unit_test_page() {
 }
 
 function akismet_unit_tests() {
+	
+	if ( !WP_DEBUG ) {
+?>
+<div class="wrap">
+<h2><?php _e('Warning'); ?></h2>
+<div class="narrow">
+<p>You must enable the WP_DEBUG constant in wp-config.php in order to run the Akismet unit tests.</p>
+<p><strong>Do not run this on a production site!</strong></p>
+<p>There is a risk of data loss or unintentional damage.  The unit tests <em>should</em> return everything to its original state, but the nature of testing means things will sometimes go wrong.</p>
+</div>
+</div>
+<?php
+		return;
+	}
+	
+	
 	require_once('simpletest/autorun.php');
 
 	$suite = new TestSuite('All Akismet tests');
