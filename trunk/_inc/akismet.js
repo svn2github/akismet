6814fb185451545d8f064a07cb00d003c2f46a77
cfinke <cfinke@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2017-05-01 15:40:48 +0000

    Remove the unnecessary URLEncode jQuery plugin.
    encodeURIComponent appears to do just as good of a job.
    
    
    git-svn-id: https://plugins.svn.wordpress.org/akismet@1649044 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/_inc/akismet.js
+++ trunk/_inc/akismet.js
@@ -99,7 +99,7 @@ jQuery( function ( $ ) {
 		clearTimeout( mshotSecondTryTimer );
 		clearTimeout( mshotThirdTryTimer );
 
-		var thisHref = $.URLEncode( $( this ).attr( 'href' ) );
+		var thisHref = encodeURIComponent( $( this ).attr( 'href' ) );
 
 		var mShot = $( '<div class="akismet-mshot mshot-container"><div class="mshot-arrow"></div><img src="//s0.wordpress.com/mshots/v1/' + thisHref + '?w=450" width="450" height="338" class="mshot-image" /></div>' );
 		mShot.data( 'link', this );
@@ -223,12 +223,4 @@ jQuery( function ( $ ) {
 			}
 		});
 	}
-});
-
-// URL encode plugin
-jQuery.extend({URLEncode:function(c){var o='';var x=0;c=c.toString();var r=/(^[a-zA-Z0-9_.]*)/;
-  while(x<c.length){var m=r.exec(c.substr(x));
-    if(m!=null && m.length>1 && m[1]!=''){o+=m[1];x+=m[1].length;
-    }else{if(c[x]==' ')o+='+';else{var d=c.charCodeAt(x);var h=d.toString(16);
-    o+='%'+(h.length<2?'0':'')+h.toUpperCase();}x++;}}return o;}
 });
